5bd84e736b2ea2b1b32e21dc20961ff326d4f8d1
Gledson A. <g_a6@live.com>
2026-06-06 16:20:55 -0300

    refact(repo): removing segregation between course and tutorial apps

--- React/tutorials/tic-tac-toe/src/components/Square.js
+++
@@ -1,3 +0,0 @@
-export default function Square({ row, column, matrix, onSquareClickHandler }) {
-  return <button onClick={() => onSquareClickHandler(row, column)} className="square">{ matrix[row][column] }</button>;
-}
