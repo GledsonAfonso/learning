eb22303d67ef56bdf97f043d0a153e5192adf010
Gledson A. <g_a6@live.com>
2026-06-14 17:51:23 -0300

    refac(react - clock): using useEffect hook to manage the time interval

--- React/clock/src/components/Clock/Clock.tsx
+++ React/clock/src/components/Clock/Clock.tsx
@@ -1,17 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const Clock = () => {
   console.log('Rendering Clock component');
 
   const [date, setDate] = useState(new Date());
 
-  setInterval(() => {
-    setDate(() => new Date());
-  }, 1000);
+  useEffect(() => {
+    console.log('Render finished, triggering useEffect.');
+
+    const intervalId = setInterval(() => {
+      setDate(() => new Date());
+    }, 1000);
+
+    return () => {
+      console.log('Unmounting component, triggering useEffect cleanup.');
+
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const addLeadingZeroes = (num: number) => {
+    return String(num).padStart(2, '0');
+  };
+
+  const getFormattedTime = () => {
+    const hours = addLeadingZeroes(date.getHours());
+    const minutes = addLeadingZeroes(date.getMinutes());
+    const seconds = addLeadingZeroes(date.getSeconds());
+
+    return `${hours}:${minutes}:${seconds}`;
+  };
 
   return (
     <section id='clock'>
-      <p className='time-slot'>{`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`}</p>
+      <p className='time-slot'>{getFormattedTime()}</p>
     </section>
   );
 };
